c7f4ce80c6900259ec28ef61f9332aff9d482b29
Felipe Oliveira <felipe.henrique.f.o@gmail.com>
2021-02-16 16:45:22 -0300

    Feito o exemplo com getStaticProps buscando na API do Github

--- pages/repositorios.js
+++ pages/repositorios.js
@@ -1,24 +1,31 @@
-import axios from 'axios';
-
-export default function Repositorios(props) {
+export default function Repositorios({ repositorios }) {
   return (
     <>
-      <h1>Hello World!</h1>
-      <p>{props.repositorios}</p>
+      <h1>Meus repositorios</h1>
+      <ul>
+        {repositorios.map((repo) => (
+          <li key={repo.id}>{repo.name}</li>
+        ))}
+      </ul>
+
+      <h4>Explicação</h4>
+      <p>
+        Apesar de serem dados de uma resposta da API do Github, estes dados são
+        processados no momento de Build desta página, sendo assim gerada uma
+        página totalmente estática com esses dados.
+      </p>
     </>
   );
 }
 
-export async function getStaticsProps() {
-  const repositorios = {};
-
-  axios.get('https://api.github.com/users/felipehfo/repos').then((res) => {
-    repositorios = res.data;
-  });
+export async function getStaticProps() {
+  const response = await fetch('https://api.github.com/users/felipehfo/repos');
+  const [...repositorios] = await response.json();
 
   return {
     props: {
       repositorios
-    }
+    },
+    revalidate: 1
   };
 }
