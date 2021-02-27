a8d7b7f8da0fb1a9cfe9650f2a82ca248696b113
Felipe Oliveira <felipe.henrique.f.o@gmail.com>
2021-02-27 15:12:37 -0300

    Personalizado a página sobre

--- pages/sobre.js
+++ pages/sobre.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Sobre() {
   return (
@@ -7,20 +8,21 @@ export default function Sobre() {
         <h3 className="title">Sobre</h3>
       </header>
       <main className="container">
-        <h2>Olá, eu sou</h2>
-        <h1>Felipe Henrique Fonseca Oliveira</h1>
-        <h4>Estudante de Ciência da Computação e Desenvolvedor Junior</h4>
-        <p>
+        <h1 className="author">Felipe Henrique Fonseca Oliveira</h1>
+        <h4 className="sub-author">
+          Estudante de Ciência da Computação e Desenvolvedor Junior
+        </h4>
+        <p className="text">
           Hoje aos meus 25 anos sou estudante de Ciência da Computação na
           Pontifícia Universidade Católica de Minas Gerais e Desenvolvedor
           Junior na Empresa Medsystem.
         </p>
-        <p>
+        <p className="text">
           Minha paixão pelo desenvolvimento web começou no início de 2019,
           sempre gostei de programar desde linguagens de baixo nível como C até
           as de alto nível como C# e Java mas nada voltado para web.
         </p>
-        <p>
+        <p className="text">
           Em 2019 por conta de uma matéria na faculdade precisei expandir meus
           conhecimentos no desenvolvimento web e foi uma viajem sem volta.
           Acabei me deparando e me apaixonando por JavaScript, uma linguagem
@@ -29,16 +31,25 @@ export default function Sobre() {
           dentro de mim quando conheci o Desenvolvimento Web.
         </p>
 
-        <h4>Links</h4>
-        <p>Facebook</p>
-        <p>Instagram</p>
-        <p>LinkedIn</p>
-        <p>Github</p>
+        <h4 className="subtitle">Links</h4>
+        <div className="container-social">
+          <Link href="/">
+            <div className="icon-facebook"></div>
+          </Link>
+          <Link href="/">
+            <div className="icon-github"></div>
+          </Link>
+          <Link href="/">
+            <div className="icon-linkedin"></div>
+          </Link>
+        </div>
 
-        <h3>Caso não tenha visto</h3>
-        <p>Abaixo segue o link onde explico mais deste projeto.</p>
+        <h3 className="subtitle">Caso não tenha visto</h3>
+        <p className="text">
+          Abaixo segue o link onde explico mais deste projeto.
+        </p>
         <Link href="exemplos">
-          <a className="link">Conhecndo um pouco do NextJS</a>
+          <a className="link">Conhecendo um pouco do NextJS</a>
         </Link>
       </main>
     </>
